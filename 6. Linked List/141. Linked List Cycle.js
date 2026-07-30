443942b41e154320086625ac221bfe97507d26d5
rahulfrontend1-cmd <rahul.frontend1@gmail.com>
2026-07-30 06:47:55 +0530

    141. Linked List Cycle

--- 6. Linked List/141. Linked List Cycle.js
+++ 6. Linked List/141. Linked List Cycle.js
@@ -61,3 +61,27 @@ var hasCycle = function(head) {
 };
 
 // O(n) time complexity and O(1) space complexity
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            return true;
+        }
+    }
+    return false;
+};

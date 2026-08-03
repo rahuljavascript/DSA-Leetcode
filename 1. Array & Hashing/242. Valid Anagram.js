05a65318e0751943326332e126a3a67ef968a41d
rahulfrontend1-cmd <rahul.frontend1@gmail.com>
2026-08-03 10:00:47 +0530

    242. Valid Anagram using Map

--- 1. Array & Hashing/242. Valid Anagram.js
+++ 1. Array & Hashing/242. Valid Anagram.js
@@ -1,3 +1,27 @@
+// 242. Valid Anagram
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "rat", t = "car"
+
+// Output: false
+
+ 
+// Constraints:
+
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
+ 
+
+// Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
 /**
  * @param {string} s
  * @param {string} t
@@ -20,4 +44,24 @@ var isAnagram = function (s, t) {
     }
     return true;
             
+};
+
+// Using Map instead of Object
+
+var isAnagramUsingMap = function (s, t) {
+    if (s.length === t.length) {
+        let map = new Map();
+        for (let i = 0; i < s.length; i++) {
+            map.set(s[i], (map.get(s[i]) || 0) + 1);
+            map.set(t[i], (map.get(t[i]) || 0) - 1);
+        }
+        
+        for (let val of map.values()) {
+            if (val !== 0) {
+                return false
+            }
+        }
+        return true
+    }
+    return false;
 };
